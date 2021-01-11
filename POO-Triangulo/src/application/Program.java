c8a0ea3e86340665e2d65a0c9fda1d28da60ef24
fern <felipe.ghidini@gft.com>
2021-01-11 13:28:48 -0300

    Exercicios de POO GFT, e cadastro de vendas Nelio Alves

---
+++ POO-Triangulo/src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangulo;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangulo x = new Triangulo();
+		Triangulo y = new Triangulo();
+		
+		System.out.println("Entre com as medidas do triangulo X");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Ente com as medidas do triangulo Y");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double areaX = x.area();
+		double areaY = y.area();
+
+		System.out.printf("Area do triângulo X: %.4f%n", areaX );
+		
+		System.out.printf("Area do triângulo Y: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Triangulo X tem a maior área.");
+		}else {
+			System.out.println("Triangulo Y tem a maior área.");
+		}
+
+		
+		sc.close();
+
+	}
+
+}
